d108586301fb5b658c7bf22149f0ff1e8e5cc6de
Édouard Gilbert <edouard@egilbert.eu>
2016-09-20 11:48:00 +0200

    Discovered @Embedable with Adresse, embedded in Employeur and Employe

---
+++ src/jpatest/entity/Adresse.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jpatest.entity;
+
+import javax.persistence.Embeddable;
+
+/**
+ *
+ * @author gilbert
+ */
+@Embeddable
+public class Adresse {
+    
+    private String rue;
+    private Integer numero;
+    private Integer codePostal;
+    private String ville;
+    private String pays;
+}
